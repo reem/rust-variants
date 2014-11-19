c27a90799b4898cc1a63bf42652f1e64f6201914
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-18 20:16:17 -0800

    (feat) Reorganize because of namespaced enums.

--- src/lib.rs
+++ src/lib.rs
@@ -6,72 +6,60 @@
 #[cfg(feature = "serialize")]
 extern crate serialize;
 
-pub mod two {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B> {
-        First(A),
-        Second(B)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Two<A, B> {
+    First(A),
+    Second(B)
 }
 
-pub mod three {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B, C> {
-        First(A),
-        Second(B),
-        Third(C)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Three<A, B, C> {
+    First(A),
+    Second(B),
+    Third(C)
 }
 
-pub mod four {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B, C, D> {
-        First(A),
-        Second(B),
-        Third(C),
-        Fourth(D)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Four<A, B, C, D> {
+    First(A),
+    Second(B),
+    Third(C),
+    Fourth(D)
 }
 
-pub mod five {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B, C, D, E> {
-        First(A),
-        Second(B),
-        Third(C),
-        Fourth(D),
-        Fifth(E)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Five<A, B, C, D, E> {
+    First(A),
+    Second(B),
+    Third(C),
+    Fourth(D),
+    Fifth(E)
 }
 
-pub mod six {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B, C, D, E, F> {
-        First(A),
-        Second(B),
-        Third(C),
-        Fourth(D),
-        Fifth(E),
-        Sixth(F)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Six<A, B, C, D, E, F> {
+    First(A),
+    Second(B),
+    Third(C),
+    Fourth(D),
+    Fifth(E),
+    Sixth(F)
 }
 
-pub mod seven {
-    #[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
-    #[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
-    pub enum Either<A, B, C, D, E, F, G> {
-        First(A),
-        Second(B),
-        Third(C),
-        Fourth(D),
-        Fifth(E),
-        Sixth(F),
-        Seventh(G)
-    }
+#[deriving(Show, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Rand)]
+#[cfg_attr(feature = "serialize", deriving(Encodable, Decodable))]
+pub enum Seven<A, B, C, D, E, F, G> {
+    First(A),
+    Second(B),
+    Third(C),
+    Fourth(D),
+    Fifth(E),
+    Sixth(F),
+    Seventh(G)
 }
 
